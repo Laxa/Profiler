ce702bec0cf163a57d4abec398ad05361522701a
laxa <laxa@ddracepro.net>
2016-02-22 11:06:16 +0100

    Should be final architecture

---
+++ Profiler/Profiler/Context.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiler
+{
+    /// <summary>
+    /// This is our internal profiler class where we are going to handle all the informations that need to be kep
+    /// This is a singleton
+    /// </summary>
+    internal sealed class Context
+    {
+        private static volatile Context instance;
+        // We are going to use this as a lock in case of multi-threading
+        private static object syncRoot = new Object();
+
+        private Context() { }
+
+        public static Context Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Context();
+                    }
+                }
+                return instance;
+            }
+        }
+
+
+    }
+}
---
+++ Profiler/Profiler/Scope.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiler
+{
+    /// <summary>
+    /// The public implementation of our scope that we will return
+    /// </summary>
+    public sealed class Scope : IScope
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
