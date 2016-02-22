e243bb5702f961db130fe34ae9afc7797baa7436
laxa <laxa@ddracepro.net>
2016-02-22 15:51:27 +0100

    Adding forgotten file, thanks VS 2015

---
+++ Profiler/Tests/LoadTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Profiler;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class LoadTests
+    {
+        /// <summary>
+        /// Testing the load of 1000 scopes
+        /// </summary>
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<IScope> scopes = new List<IScope>();
+            for (int i = 0; i < 1000; i++)
+            {
+                scopes.Add(Profiler.Profiler.Start($"scope{i}"));
+            }
+            for (int i = 0; i < 1000; i++)
+            {
+                scopes[i].Stop();
+            }
+            for (int i = 0; i < 1000; i++)
+            {
+                Profiler.Profiler.Close($"scope{i}");
+            }
+        }
+
+        /// <summary>
+        /// Testing the load of 10000 scopes
+        /// </summary>
+        [TestMethod]
+        public void LoadTestTwo()
+        {
+            List<IScope> scopes = new List<IScope>();
+            for (int i = 0; i < 10000; i++)
+            {
+                scopes.Add(Profiler.Profiler.Start($"scope{i}"));
+            }
+            for (int i = 0; i < 10000; i++)
+            {
+                scopes[i].Stop();
+            }
+            for (int i = 0; i < 10000; i++)
+            {
+                Profiler.Profiler.Close($"scope{i}");
+            }
+        }
+
+        /// <summary>
+        /// Testing the load of 1000 scopes and exporting to CSV
+        /// </summary>
+        [TestMethod]
+        public void LoadTestAndExport()
+        {
+            List<IScope> scopes = new List<IScope>();
+            for (int i = 0; i < 1000; i++)
+            {
+                scopes.Add(Profiler.Profiler.Start($"scope{i}"));
+            }
+            for (int i = 0; i < 1000; i++)
+            {
+                scopes[i].Stop();
+            }
+            for (int i = 0; i < 1000; i++)
+            {
+                Profiler.Profiler.Close($"scope{i}");
+            }
+            Profiler.Profiler.ExportCsv();
+        }
+
+        /// <summary>
+        /// Testing the load of 10000 scopes and exporting to CSV
+        /// </summary>
+        [TestMethod]
+        public void LoadTestAndExportTwo()
+        {
+            List<IScope> scopes = new List<IScope>();
+            for (int i = 0; i < 10000; i++)
+            {
+                scopes.Add(Profiler.Profiler.Start($"scope{i}"));
+            }
+            for (int i = 0; i < 10000; i++)
+            {
+                scopes[i].Stop();
+            }
+            for (int i = 0; i < 10000; i++)
+            {
+                Profiler.Profiler.Close($"scope{i}");
+            }
+            Profiler.Profiler.ExportCsv();
+        }
+    }
+}
---
+++ Profiler/Tests/SpecialIScope.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Profiler;
+
+namespace Tests
+{
+    [TestClass]
+    public class SpecialIScope
+    {
+        /// <summary>
+        /// Testing that we don't crash on other implementation of IScope
+        /// </summary>
+        [TestMethod]
+        public void SpecialIScopeTest()
+        {
+            SpecialScope t = new SpecialScope();
+            Profiler.Profiler.Close(t);
+        }
+    }
+
+    public class SpecialScope : IScope
+    {
+        public string TestVariable { get; set; }
+
+        public SpecialScope()
+        {
+            TestVariable = nameof(SpecialScope);
+        }
+
+        public void Start()
+        {
+            // do stuff
+        }
+
+        public void Stop()
+        {
+            // do stuff
+        }
+    }
+}
